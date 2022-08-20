825c33db94aa4ad2755364f4d3def115d5632cd6
agarwalshashankjan <agarwalshashank.jan>
2022-08-20 17:35:44 +0530

    Added User profile Route and Controller

---
+++ controllers/users_controller.js
@@ -0,0 +1,7 @@
+module.exports.profile = function (req, res) {
+  return res.end("<h1>User profile</h1>");
+};
+
+module.exports.posts = function (req, res) {
+  return res.end("<h1>User posts</h1>");
+};
