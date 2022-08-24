b7cd40a8abdf91d7036726ac4fa500234042c6c9
agarwalshashankjan <agarwalshashank.jan>
2022-08-24 14:13:50 +0530

    Added common styling for Layout and individual styling for each page will render

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
 app.use(expressLayouts);
+app.use(express.static("./assets"));
 
 app.use("/", require("./routes"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 app.listen(port, function (error) {
   if (error) {
